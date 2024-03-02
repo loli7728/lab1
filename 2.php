36034adb54eaeed8c5b579435b44cbaa10af67d3
name <nikityz2281@gmail.com>
2024-03-02 15:18:23 +0300

    edit

--- 2.php
+++ 2.php
@@ -3,8 +3,8 @@
 $color1 = readline("Введите первый цвет: ");
 $color2 = readline("Введите второй цвет: ");
 
-if ($color1 === $color1) {
-    echo "$color1";
+if ($color1 === $color2) {
+    echo $color1;
     exit;
 }
 if (($color1 == "красный" && $color2 == "синий") || ($color2 == "красный" && $color1 == "синий")) {
